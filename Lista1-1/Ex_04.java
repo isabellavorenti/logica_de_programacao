7f6788be16f591c9c5186197b545f4d0c8355c75
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:32:05 -0300

    Ex_04.java

--- Lista1-1/Ex_04.java
+++ Lista1-1/Ex_04.java
@@ -1,8 +1,12 @@
 
 import java.util.Scanner;
 
-public class Lt_1_Ex_04 {
-    public static void main (String args []) {
+public class Lt_1_Ex_04 
+{
+    
+    public static void main (String args []) 
+    {
+        
         double tempC, tempF;
         
         Scanner ler = new Scanner (System.in);
@@ -10,8 +14,10 @@ public class Lt_1_Ex_04 {
         System.out.println("Digite a temperatura em Celsius: ");
         tempC = ler.nextDouble();
         
-        tempF = (9 * tempC + 160) / 5;
-        
+        tempF = ((9 * tempC + 160) / 5);
+
         System.out.println("Temperatura convertida em Fahrenheit: " + tempF + "°F");
+        
     }
+    
 }
