739968916ce91b460dd1ffbabe786228dd520e0a
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:51:27 -0300

    Ex_17.java

--- Lista1-1/Ex_17.java
+++ Lista1-1/Ex_17.java
@@ -2,9 +2,12 @@ package Aulas;
 
 import java.util.Scanner;
 
-public class _Lt01_17 {
+public class Ex_17 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		double vm, t, e, qtde;
 		
 		Scanner ler = new Scanner (System.in);
@@ -15,11 +18,12 @@ public class _Lt01_17 {
 		System.out.println("Insira a velocidade média em km/h: ");
 		vm = ler.nextDouble();
 		
-		e = vm * t;
+		e = (vm * t);
 		
-		qtde = e / 12;
+		qtde = (e / 12);
 				
 		System.out.println("Quantidade de litros gastos na viagem = " + qtde);
+		
 	}
 
 }
