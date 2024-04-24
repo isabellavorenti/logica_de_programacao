0e03aa8b65d92a9af58e19da269ad2dd9644a6e8
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:01:24 -0300

    Ex_25.java

--- Lista1-1/Ex_25.java
+++ Lista1-1/Ex_25.java
@@ -1,25 +1,34 @@
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_25 {
+public class Ex_25 
+{
     
-    public static void main (String args []) {
+    public static void main (String args []) 
+    {
+        
         double horaInicial, horaFinal, tempoTotal;
         
         horaInicial = Double.parseDouble(JOptionPane.showInputDialog("Digite o horário de início: "));
         horaFinal = Double.parseDouble(JOptionPane.showInputDialog("Digite o horário final do jogo: "));
         
-        if (horaFinal < horaInicial) {
+        if (horaFinal < horaInicial) 
+        {
             tempoTotal = (24 - horaInicial) + horaFinal;
             JOptionPane.showMessageDialog(null, "O tempo total do jogo foi de " + tempoTotal + " horas.");
-
-        } else if (horaInicial < horaFinal) {
+        } 
+        
+        else if (horaInicial < horaFinal)
+        {
             tempoTotal = horaFinal - horaInicial;
-            JOptionPane.showMessageDialog(null, "O tempo total do jogo foi de " + tempoTotal + " horas.");
-            
-        } else {
+            JOptionPane.showMessageDialog(null, "O tempo total do jogo foi de " + tempoTotal + " horas.");            
+        } 
+        
+        else
+        {
             JOptionPane.showMessageDialog(null, "O jogo durou 24 horas.");
         }
         
     }
+    
 }
