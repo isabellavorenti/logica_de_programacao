8559ab8dff407777f0acca6305d38d155e167532
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:21:14 -0300

    Ex_36.java

--- Lista1-1/Ex_36.java
+++ Lista1-1/Ex_36.java
@@ -1,29 +1,31 @@
-package Ex;
 
 import javax.swing.JOptionPane;
 
-public class Ex_36 {
+public class Ex_36
+{
 
-	public static void main(String[] args) {
-		 double soma, div, n, fat;
-		                                                                          
-		 fat = 1;
-		 soma = 1;
-		 n = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro: "));
+	public static void main(String[] args) 
+	{
+		
+		 double soma = 1, div, n, fat = 1;
+		    
+		 n = Integer.parseInt(JOptionPane.showInputDialog("Digite um nÃºmero inteiro: "));
 		 
-		 for (int i = 1; i <= n; i++) {
-			 for (int j = 1; j <= i; j++) {
-				fat = fat * j;
-				
+		 for (int i = 1; i <= n; i++)
+		 {
+		 	for (int j = 1; j <= i; j++) 
+			{
+				fat *= j;
 			}
 			 
-			div = 1/fat;
-			soma = div + soma;
+			div = (1 / fat);
+			soma += div;
 			fat = 1;
 		} 
 		
 		System.out.println("n = " + n);
 		System.out.println("Total = " + soma);
+		
 	}
 
 }
