beee4f3da6365a456ff366b70679495059a7f1d0
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:52:55 -0300

    Ex_18.java

--- Lista1-1/Ex_18.java
+++ Lista1-1/Ex_18.java
@@ -1,24 +1,34 @@
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_18 {
+public class Ex_18 
+{
     
-    public static void main(String args []){
+    public static void main(String args [])
+    {
+        
         int a, b;
         
         a = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor de a:"));
         b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de b:"));
         
-        if (a > b) {
+        if (a > b) 
+        {
             a -= b;
             JOptionPane.showMessageDialog(null, "Resultado da subtração = " + a);
-        } else if (b > a){
+        } 
+        
+        else if (b > a)
+        {
             b -= a;
             JOptionPane.showMessageDialog(null, "Resultado da subtração = " + b);
-            
-        } else {
+        } 
+        
+        else 
+        {
             JOptionPane.showMessageDialog(null, a + " = " + b);
         }
-}
+        
+    }
             
 }
