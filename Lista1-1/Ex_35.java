3b4e52a308b819e9f67a2e71af3093e5981827b6
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:18:35 -0300

    Ex_35.java

--- Lista1-1/Ex_35.java
+++ Lista1-1/Ex_35.java
@@ -1,10 +1,12 @@
-package Lt01_31_45;
 
 import java.util.Scanner;
 
-public class Ex_35 {
+public class Ex_35
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		int x, y, soma = 0;
 		
 		Scanner ler = new Scanner (System.in);
@@ -15,31 +17,31 @@ public class Ex_35 {
 		System.out.println("Digite o segundo valor: ");
 		y = ler.nextInt();
 		
-		if (x > y) {
-			for (int i = 0; i <= x; i++) {
-				if ((x % 3 == 0) && (y % 3 == 0)) {
-					soma = soma + (x + y) ;
-				} else {
-					soma = soma + 0;
-				}
+		if (x > y) 
+		{
+			for (int i = y; i <= x; i++)
+			{
+				if (i % 2 != 0) 
+				{
+					soma += i;
+				} 
 			}
-			
-		} else if ((y > x) || (x == y)){
-			for (int i = 0; i <= y; i++) {
-				if ((x % 3 == 0) && (y % 3 == 0)) {
-					soma = soma + (x + y) ;
-				} else {
-					soma = soma + 0;
-				}
-		
-
 		}
-	}
+		
+		else if ((y > x))
+		{
+			for (int i = x; i <= y; i++) 
+			{
+				if (i % 2 != 0) 
+				{
+					soma += i;
+				} 
+			}
+		}
 		
 		System.out.println("Resultado da soma: " + soma);
-}
-	
-	
+		
+	}
 	
 }
 		
