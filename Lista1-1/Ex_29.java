559dd6552e0224af3673f28684790c3b0e3fa69c
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:07:58 -0300

    Ex_29.java

--- Lista1-1/Ex_29.java
+++ Lista1-1/Ex_29.java
@@ -2,27 +2,36 @@ package Exercicios;
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_29 {
+public class Ex_29 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		int tipo;
 		double valorInv, valorNovo;
 
 		tipo = Integer.parseInt(JOptionPane.showInputDialog("Digite o número do tipo de investimento:"
-															+ "\n1 - Poupança \n2 - Renda Fixa"));
+									+ "\n1 - Poupança \n2 - Renda Fixa"));
 		valorInv = Double.parseDouble(JOptionPane.showInputDialog("Digite o valor investido: "));
 		
-		if (tipo == 1) {
-			valorNovo = valorInv + (valorInv * 0.03);
+		if (tipo == 1) 
+		{
+			valorNovo = (valorInv + (valorInv * 0.03));
 			JOptionPane.showMessageDialog(null, "Valor do Investimento em Poupança após 30 dias: R$ " + valorNovo);
-			
-		} else if (tipo == 2){
-			valorNovo = valorInv + (valorInv * 0.05);
+		} 
+		
+		else if (tipo == 2)
+		{
+			valorNovo = (valorInv + (valorInv * 0.05));
 			JOptionPane.showMessageDialog(null, "Valor do Investimento em Renda Fixa após 30 dias: R$ " + valorNovo);
-
-		} else {
+		} 
+		
+		else 
+		{
 			JOptionPane.showMessageDialog(null, "O valor de investimento fornecido é inexistente.");
 		}
+		
 	}
 
 }
