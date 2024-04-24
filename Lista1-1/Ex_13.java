0a4cf262b5f77636c7e58444cccbe89ff54b502d
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:46:10 -0300

    Ex_13.java

--- Lista1-1/Ex_13.java
+++ Lista1-1/Ex_13.java
@@ -2,14 +2,17 @@ package Exercicios;
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_13 {
+public class Ex_13 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		double alimento, duracao;
 		
 		alimento = Double.parseDouble(JOptionPane.showInputDialog("Digite a quantidade de alimento em quilos: "));
 		
-		duracao = (alimento * 1000) / 50;
+		duracao = ((alimento * 1000) / 50);
 		
 		JOptionPane.showMessageDialog(null, "O alimento durará " + duracao + " dias.");
 
