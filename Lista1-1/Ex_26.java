008db5b85be8352cbbfa51e638334a0c830ccec6
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:02:46 -0300

    Ex_26.java

--- Lista1-1/Ex_26.java
+++ Lista1-1/Ex_26.java
@@ -1,32 +1,46 @@
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_26 {
+public class Ex_26 
+{
     
-    public static void main (String args []) {
+    public static void main (String args []) 
+    {
         
         int a, b, div;
         
         a = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro: "));
         b = Integer.parseInt(JOptionPane.showInputDialog("Digite um segundo valor inteiro: "));
         
-        if (a > b) {
-            if (a % b == 0) {
+        if (a > b) 
+        {
+            if (a % b == 0) 
+            {
                 div = a / b;
                 JOptionPane.showMessageDialog(null, "O valor " + a + " é múltiplo de " + b + "\nDivisão = " + div);
+            } 
             
-            } else {
+            else 
+            {
                 JOptionPane.showMessageDialog(null, "O valor " + a + " não é múltiplo de " + b);
             } 
-        } else {
-            if (b % a == 0) {
+        }
+        
+        else 
+        {
+            if (b % a == 0)
+            {
                 div = b / a;
                 JOptionPane.showMessageDialog(null, "O valor " + b + " é múltiplo de " + a + "\nDivisão = " + div);
-           
-            } else {
-                JOptionPane.showMessageDialog(null, "O valor " + b + " não é múltiplo de " + a);
-                
+            } 
+            
+            else 
+            {
+                JOptionPane.showMessageDialog(null, "O valor " + b + " não é múltiplo de " + a); 
             }
+            
         }
+        
     }
+    
 }
