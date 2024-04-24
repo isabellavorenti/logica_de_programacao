b18d6cca585fd3287f05986ade10986f98d63701
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:10:29 -0300

    Ex_33.java

--- Lista1-1/Ex_33.java
+++ Lista1-1/Ex_33.java
@@ -1,20 +1,22 @@
-package Lt01_31_45;
 
 import java.util.Scanner;
 
-public class Ex_33 {
+public class Ex_33
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		int n, serie;
 		double soma = 1;
 		
 		Scanner ler = new Scanner (System.in);
 		
-		System.out.println("Digite um número: ");
+		System.out.println("Digite um nÃºmero: ");
 		n = ler.nextInt();
 		
-		for (serie = 1;  serie <= n ; serie ++) {
-			
+		for (serie = 1;  serie <= n ; serie ++) 
+		{
 			System.out.println(soma);
 		}
 
