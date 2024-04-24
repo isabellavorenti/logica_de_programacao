f6cb31ddf6dda9736a2aa72dbcf8a7abb0008656
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:32:17 -0300

    Ex_38.java

--- Lista1-1/Ex_38.java
+++ Lista1-1/Ex_38.java
@@ -1,35 +1,44 @@
-package Vetor_21_03_24;
 
 import javax.swing.JOptionPane;
 
-    public class Ex38 {
-        public static void main(String [] args) {
+public class Ex_38 
+{
+        
+    public static void main(String [] args) 
+    {
             
+        int valor = 0, num;
+        double maior = 0, menor = 0;
             
-            int valor;
-            double maior, menor, num;
+        for (int i = 0; i < 100; i++) 
+        {
             
-            maior = 0;
-            menor = 0;
-            valor = 0;
+            num = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor: "));
             
-            for (int i = 0; i < 10; i++) {
-                num = Integer.parseInt(JOptionPane.showInputDialog("Digite o número da posição " + i + ": "));
-                if (num>0) {
-                    valor = valor + 1;
-                    if (valor == 1) {
-                        maior = num;
-                        menor = num;
-                        
-                    } else if (num > maior){
-                        maior = num;
-                        
-                    } else if (num < menor){
-                        menor = num;
-                    }
+            if (num > 0) 
+            {
+                valor += 1;
+                
+                if (valor == 1) 
+                {
+                    maior = num;
+                    menor = num;  
+                } 
+                
+                else if (num > maior)
+                {
+                    maior = num;     
+                } 
+                
+                else if (num < menor)
+                {
+                    menor = num;
                 }
             }
-            JOptionPane.showMessageDialog(null, "Maior número: " + maior + "\nMenor número: " + menor);
         }
+        
+        JOptionPane.showMessageDialog(null, "Maior número: " + maior + "\nMenor número: " + menor);
+        
+    }
     
 }
