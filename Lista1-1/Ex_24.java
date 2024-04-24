e78006f63a7832d457652074c1e88ac5b5ba09c9
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:00:33 -0300

    Ex_24.java

--- Lista1-1/Ex_24.java
+++ Lista1-1/Ex_24.java
@@ -1,22 +1,29 @@
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_24 {
+public class Ex_24
+{
     
-    public static void main (String args []) {
+    public static void main (String args [])
+    {
+        
         int n, v1, v2;
         
         n = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro: "));
         
-        if ((n % 2 == 0) && (n % 3 == 0)) {
+        if ((n % 2 == 0) && (n % 3 == 0)) 
+        {
             v1 = n / 2;
             v2 = n / 3;
             JOptionPane.showMessageDialog(null, "O valor " + n + " é divisível por 2 e 3. "
                                           + "\nDivisão por 2 = " + v1 + "\nDivisão por 3 = " + v2);
-            
-        } else {
+        }
+        
+        else 
+        {
             JOptionPane.showMessageDialog(null, "O valor " + n + " não é dívisível por 2 e 3.");
         }
         
     }
+    
 }
