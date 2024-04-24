277ad19b9b31d0f86075322aa477ae29189344d8
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:29:43 -0300

    Ex_02.java

--- Lista1-1/Ex_02.java
+++ Lista1-1/Ex_02.java
@@ -1,8 +1,10 @@
 
 import java.util.Scanner;
 
-public class Lt_1_Ex_02 {
-    public static void main (String args []) {
+public class Lt_1_Ex_02 
+{
+    public static void main (String args []) 
+    {
         double salario, reajuste;
         
         Scanner ler = new Scanner (System.in);
@@ -10,8 +12,9 @@ public class Lt_1_Ex_02 {
         System.out.println("Entre com o valor do salário: ");
         salario = ler.nextDouble();
         
-        reajuste = salario + (salario *0.15);
+        reajuste = salario + (salario * 0.15);
         
         System.out.println("Novo salário com reajuste: R$" + reajuste);
+        
     }
 }
