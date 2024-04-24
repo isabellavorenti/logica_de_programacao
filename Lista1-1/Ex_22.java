0b13d87ed0bb9b60fc847bc8773b47e72bfb1074
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:57:58 -0300

    Ex_22.java

--- Lista1-1/Ex_22.java
+++ Lista1-1/Ex_22.java
@@ -1,22 +1,32 @@
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_22 {
+public class Ex_22 
+{
     
-    public static void main (String args []) {
+    public static void main (String args []) 
+    {
+        
         int n1, n2;
         
         n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro valor: "));
         n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo valor: "));
         
-        if (n1 > n2) {
+        if (n1 > n2)
+        {
             JOptionPane.showMessageDialog(null, "Ordem crescente dos valores: " + n2 + ", " + n1);
-            
-        } else if (n1 < n2){
-            JOptionPane.showMessageDialog(null, "Ordem crescente dos valores: " + n1 + ", " + n2);
+        } 
         
-        } else {
+        else if (n1 < n2)
+        {
+            JOptionPane.showMessageDialog(null, "Ordem crescente dos valores: " + n1 + ", " + n2);
+        } 
+        
+        else 
+        {
             JOptionPane.showMessageDialog(null, "Valores iguais.");
         }
+        
     }
+    
 }
