43219f5b44374117581f7280cb3d2b9473a81082
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:41:57 -0300

    Ex_10.java

--- Lista1-1/Ex_10.java
+++ Lista1-1/Ex_10.java
@@ -1,19 +1,21 @@
-package Exercicios;
-
 import javax.swing.JOptionPane;
-import javax.swing.JPanel;
 
-public class _Lt01_10 {
+public class Ex_10
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		
 		double a, b, sub;
 		
 		a = Double.parseDouble(JOptionPane.showInputDialog("Digite o primeiro valor: "));
+		
 		b = Double.parseDouble(JOptionPane.showInputDialog("Digite o segundo valor: "));
 		
-		sub = a - b;
+		sub = (a - b);
 		
 		JOptionPane.showMessageDialog(null, "Resultado da subtração = " + sub);
+		
 	}
 
 }
