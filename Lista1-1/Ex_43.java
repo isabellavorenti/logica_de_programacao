335564c440d81a26f12afd1720973993bf026a2b
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:41:08 -0300

    Ex_43.java

--- Lista1-1/Ex_43.java
+++ Lista1-1/Ex_43.java
@@ -1,27 +1,22 @@
 package Atividades;
 
-public class Ex_43 {
-
+public class Ex_43 
+{
 	public static void main(String[] args)
 	{
-		
-		double altAna, altMaria, anos;
+		double altAna, altMaria, anos = 0;
 		
 		altAna = 1.1;
 		altMaria = 1.5;
-		anos = 0;
 		
 		while (altAna <= altMaria) 
 		{
-			
 			anos += 1;
 			altAna += 0.03;
 			altMaria += 0.02;
-			
 		}
 		
 		System.out.println("Altura Ana: " + altAna + "m" + "\nAltura Maria: " + altMaria + "m\nAnos: " + anos);
 
 	}
-
 }
