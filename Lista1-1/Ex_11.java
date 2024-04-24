e070732aedeb224e67d3b4b767acf2fb472caa8c
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:43:01 -0300

    Ex_11.java

--- Lista1-1/Ex_11.java
+++ Lista1-1/Ex_11.java
@@ -1,15 +1,17 @@
-package Exercicios;
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_11 {
+public class Ex_11 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		double raio, comp;
 		
 		raio = Double.parseDouble(JOptionPane.showInputDialog("Insira o valor do raio: "));
 		
-		comp = raio * 2 * Math.PI;
+		comp = (raio * 2 * Math.PI);
 		
 		JOptionPane.showMessageDialog(null, "Valor do comprimento = " + comp);
 
