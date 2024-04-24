6b836c0580316a27db8210ea0dba0707cb74bcab
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:11:13 -0300

    Ex_34.java

--- Lista1-1/Ex_34.java
+++ Lista1-1/Ex_34.java
@@ -1,18 +1,21 @@
-package Lt01_31_45;
 
 import java.util.Scanner;
 
-public class Ex_34 {
+public class Ex_34
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		int mult, n;
 		
 		Scanner ler = new Scanner (System.in);
 		
-		System.out.println("Digite um número: ");
+		System.out.println("Digite um nÃºmero: ");
 		n = ler.nextInt();
 		
-		for (int i = 0; i <= 10; i++) {
+		for (int i = 0; i <= 10; i++) 
+		{
 			mult = (n * i);
 			System.out.println(n + " x " + i + " = " + mult);
 		}
