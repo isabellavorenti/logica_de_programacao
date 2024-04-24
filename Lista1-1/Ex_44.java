75c3c7c52679a252fe01bddbec21315d9be5a1b3
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:37:17 -0300

    Ex_44.java

--- Lista1-1/Ex_44.java
+++ Lista1-1/Ex_44.java
@@ -4,10 +4,8 @@ import javax.swing.JOptionPane;
 
 public class Ex_44 
 {
-
 	public static void main(String[] args) 
 	{
-		
 		double base, exp, pot = 1;
 	
 		base = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da base: "));
@@ -15,13 +13,10 @@ public class Ex_44
 		
 		for (int i = 0; i < exp; i++)
 		{
-			
 			pot = (pot * base);
-			
 		}
 		
 		System.out.println("Total = " + pot);
-
+		
 	}
-
 }
