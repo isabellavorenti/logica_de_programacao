ac9abc959259df4215e3015e24869c2893bb3409
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:31:01 -0300

    Ex_03.java

--- Lista1-1/Ex_03.java
+++ Lista1-1/Ex_03.java
@@ -1,8 +1,10 @@
 
 import java.util.Scanner;
 
-public class Lt_1_Ex_03 {
-    public static void main (String args []) {
+public class Lt_1_Ex_03 
+{
+    public static void main (String args [])
+    {
         double base, altura, area;
         
         Scanner ler = new Scanner (System.in);
@@ -13,8 +15,9 @@ public class Lt_1_Ex_03 {
         System.out.println("Insira a altura: ");
         altura = ler.nextDouble();
         
-        area = (altura * base) / 2;
+        area = ((altura * base) / 2);
         
         System.out.println("Área do triângulo = " + area);
+        
     }
 }
