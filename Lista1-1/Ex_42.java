f8118009838bc7abbb88968c3f9392194e00734f
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:40:03 -0300

    Ex_42.java

--- Lista1-1/Ex_42.java
+++ Lista1-1/Ex_42.java
@@ -2,23 +2,18 @@ package Atividades;
 
 public class Ex_42
 {
-
 	public static void main(String[] args)
 	{
-
 		double div = 1, total = 0;
 		
 		for (int i = 1; i < 5; i++) 
 		{
-			
 			total = ((i / div) + total);
 			
 			div += 2;
-			
 		}
 		
 		System.out.println("Total = " + total);
 
 	}
-
 }
