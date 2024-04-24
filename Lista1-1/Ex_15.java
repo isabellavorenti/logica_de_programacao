96a742b30f451c2bd26d8d38a5663cd7da06a550
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:48:36 -0300

    Ex_15.java

--- Lista1-1/Ex_15.java
+++ Lista1-1/Ex_15.java
@@ -2,18 +2,23 @@ package Exercicios;
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_15 {
+public class Ex_15 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		
 		int c1, c2;
 		double hipotenusa;
 		
 		c1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do primeiro cateto: "));
+		
 		c2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do segundo cateto: "));
 
 		hipotenusa = (Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2)));
 		
 		JOptionPane.showMessageDialog(null, "Valor da hipotenusa = " + hipotenusa);
+		
 	}
 
 }
