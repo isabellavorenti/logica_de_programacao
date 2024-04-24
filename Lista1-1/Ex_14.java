78f985689385c7f9c4a29d4559d3fc2d7bcd36d9
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 21:47:36 -0300

    Ex_14.java

--- Lista1-1/Ex_14.java
+++ Lista1-1/Ex_14.java
@@ -2,17 +2,22 @@ package Exercicios;
 
 import javax.swing.JOptionPane;
 
-public class _Lt01_14 {
+public class Ex_14 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		
 		int a1, a2, a3;
 		
 		a1 = Integer.parseInt(JOptionPane.showInputDialog("Digite o primeiro ângulo: "));
+		
 		a2 = Integer.parseInt(JOptionPane.showInputDialog("Digite o segundo ângulo: "));
 		
-		a3 = 180 - a1 - a2;
+		a3 = (180 - a1 - a2);
 		
 		JOptionPane.showMessageDialog(null, "Ângulo 3 = " + a3 + "°");
+		
 	}
 
 }
