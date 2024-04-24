7690cec47cdd6e4b8685917ce222676f5c277842
Isabel Costa Lavorenti <167912823+isabellavorenti@users.noreply.github.com>
2024-04-23 22:41:39 -0300

    Ex_01.java

--- Lista1-1/Ex_01.java
+++ Lista1-1/Ex_01.java
@@ -1,11 +1,9 @@
 import javax.swing.JOptionPane;
 
-public class Lt_1_Ex_01
+public class Ex_01
 {
-    
     public static void main (String args [])
     {
-        
         double lado, area;
     
         lado = Double.parseDouble(JOptionPane.showInputDialog("Entre com o lado do quadrado: "));
